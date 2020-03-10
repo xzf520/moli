619362676db55c9818a3b235563b57d350e32d9b
974751082@qq.com <974751082@qq.com>
2020-03-11 03:09:41 +0800

    首次提交

---
+++ oldx-system/src/main/java/cc/oldx/modules/system/controller/IndexCtroller.java
@@ -0,0 +1,18 @@
+package cc.oldx.modules.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: moli
+ * @Email: 974751082@qq.com
+ * @qq: 974751082
+ * @Date: 2020/3/11 2:23
+ */
+@RestController
+public class IndexCtroller {
+    @RequestMapping("/hello")
+    private String hello(){
+        return "hello world";
+    }
+}
