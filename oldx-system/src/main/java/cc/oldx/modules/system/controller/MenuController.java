74f688dd715d4a628abe2c74664946833e638d6f
974751082@qq.com <974751082@qq.com>
2020-03-18 15:19:01 +0800

    增加注释

--- oldx-system/src/main/java/cc/oldx/modules/system/controller/MenuController.java
+++ oldx-system/src/main/java/cc/oldx/modules/system/controller/MenuController.java
@@ -4,7 +4,12 @@ import cc.oldx.common.api.CommonResult;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * @Author: moli
+ * @Email: 974751082@qq.com
+ * @qq: 974751082
+ * @Date: 2020/3/18 4:48
+ */
 @RestController
 public class MenuController {
     @RequestMapping("/menu/listAll")
